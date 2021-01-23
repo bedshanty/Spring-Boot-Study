55f70c981ca388b1f376a4f97d75e7b06a6f00c1
Ji Heon Lee <jihonlee@naver.com>
2021-01-24 00:26:19 +0900

    Posts 수정, 조회 추가

---
+++ src/main/java/io/makeon/springboottest/web/dto/PostsResponseDto.java
@@ -0,0 +1,19 @@
+package io.makeon.springboottest.web.dto;
+
+import io.makeon.springboottest.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
